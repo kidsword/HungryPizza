7ac25473c7019e89e81867d13538ca21bbc4a8d3
Daniel Amaral <kidsword@gmail.com>
2020-09-26 09:35:45 -0300

    Add project files.

---
+++ HungryPizza.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HungryPizza.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
